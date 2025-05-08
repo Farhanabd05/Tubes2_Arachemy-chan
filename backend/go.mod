341222a507d6062c61f496cbcbf75000a9049de7
Farhanabd05 <13523042@std.stei.itb.ac.id>
2025-05-08 21:36:36 +0700

    feat: scraper

--- backend/go.mod
+++ backend/go.mod
@@ -8,6 +8,21 @@ require (
 )
 
 require (
+	github.com/PuerkitoBio/goquery v1.10.3 // indirect
+	github.com/andybalholm/cascadia v1.3.3 // indirect
+	github.com/antchfx/htmlquery v1.3.4 // indirect
+	github.com/antchfx/xmlquery v1.4.4 // indirect
+	github.com/antchfx/xpath v1.3.3 // indirect
+	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/kennygrant/sanitize v1.2.4 // indirect
+	github.com/saintfish/chardet v0.0.0-20230101081208-5e3ef4b5456d // indirect
+	github.com/temoto/robotstxt v1.1.2 // indirect
+	google.golang.org/appengine v1.6.8 // indirect
+)
+
+require (
 	github.com/bytedance/sonic v1.13.2 // indirect
 	github.com/bytedance/sonic/loader v0.2.4 // indirect
 	github.com/cloudwego/base64x v0.1.5 // indirect
@@ -17,6 +32,7 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.26.0 // indirect
 	github.com/goccy/go-json v0.10.5 // indirect
+	github.com/gocolly/colly v1.2.0
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.10 // indirect
 	github.com/kr/text v0.2.0 // indirect
---
+++ backend/test/main.go
@@ -0,0 +1,147 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/gocolly/colly"
+)
+
+type ElementType string
+
+const (
+	Starting ElementType = "Starting"
+	Tier1    ElementType = "Tier1"
+	Tier2    ElementType = "Tier2"
+	Tier3    ElementType = "Tier3"
+	Tier4    ElementType = "Tier4"
+	Tier5    ElementType = "Tier5"
+	Tier6    ElementType = "Tier6"
+	Tier7    ElementType = "Tier7"
+	Tier8    ElementType = "Tier8"
+	Tier9    ElementType = "Tier9"
+	Tier10   ElementType = "Tier10"
+	Tier11   ElementType = "Tier11"
+	Tier12   ElementType = "Tier12"
+	Tier13   ElementType = "Tier13"
+	Tier14   ElementType = "Tier14"
+	Tier15   ElementType = "Tier15"
+)
+
+type RecipeType struct {
+	Element     string
+	ImgUrl1     string
+	ImgUrl2     string
+	Ingredient1 string
+	Ingredient2 string
+	Type        ElementType
+}
+
+func getElementType(index int) ElementType {
+	elementTypes := []ElementType{
+		Starting, "", Tier1, Tier2, Tier3, Tier4, Tier5, Tier6, Tier7,
+		Tier8, Tier9, Tier10, Tier11, Tier12, Tier13, Tier14, Tier15,
+	}
+	if index < 1 || index > len(elementTypes) {
+		return ""
+	}
+	return elementTypes[index-1]
+}
+
+func ScrapeHandler(ctx *gin.Context) {
+	url := "https://little-alchemy.fandom.com/wiki/Elements_(Little_Alchemy_2)"
+	var recipes []RecipeType
+
+	c := colly.NewCollector(colly.AllowedDomains("little-alchemy.fandom.com"))
+	tableIndex := 0
+
+	c.OnHTML("table.list-table", func(table *colly.HTMLElement) {
+		tableIndex++
+		elementType := getElementType(tableIndex)
+		if elementType == "" {
+			return
+		}
+
+		table.ForEach("tbody tr", func(_ int, h *colly.HTMLElement) {
+			element := strings.TrimSpace(h.ChildText("td:first-of-type a"))
+			if element == "" || element == "Time" || element == "Ruins" || element == "Archeologist" {
+				return
+			}
+
+			h.ForEach("td:nth-of-type(2) li", func(_ int, li *colly.HTMLElement) {
+				aTags := li.DOM.Find("a")
+
+				if aTags.Length() < 4 {
+					return
+				}
+
+				imgUrl1, _ := aTags.Eq(0).Find("img").Attr("data-src")
+				imgUrl2, _ := aTags.Eq(2).Find("img").Attr("data-src")
+				ingredient1 := strings.TrimSpace(aTags.Eq(1).Text())
+				ingredient2 := strings.TrimSpace(aTags.Eq(3).Text())
+
+				if ingredient1 == "Time" || ingredient2 == "Time" || ingredient1 == "Ruins" || ingredient2 == "Ruins" || ingredient1 == "Archeologist" || ingredient2 == "Archeologist" {
+					return
+				}
+
+				r := RecipeType{
+					Element:     element,
+					ImgUrl1:     imgUrl1,
+					ImgUrl2:     imgUrl2,
+					Ingredient1: ingredient1,
+					Ingredient2: ingredient2,
+					Type:        elementType,
+				}
+				recipes = append(recipes, r)
+			})
+		})
+	})
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Print("Visiting ", r.URL)
+	})
+
+	c.OnError(func(r *colly.Response, e error) {
+		fmt.Println("Error:", e.Error())
+	})
+
+	if err := c.Visit(url); err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	if err := os.MkdirAll("data", 0755); err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create data directory"})
+		return
+	}
+
+	filePath := "data/recipes.json"
+	jsonBytes, err := json.Marshal(recipes)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to marshal recipes to JSON"})
+		return
+	}
+	if err := os.WriteFile(filePath, jsonBytes, 0644); err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to save recipes"})
+		return
+	}
+
+	ctx.SetCookie("scraped", "true", 86400, "/", "localhost", false, true)
+	ctx.JSON(http.StatusOK, gin.H{"data": recipes})
+}
+
+func main() {
+	r := gin.Default()
+	r.Use(cors.Default())
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "pong"})
+	})
+	r.GET("/scrape", ScrapeHandler)
+	r.Run(":8080")
+}
