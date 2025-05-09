b4b71a2650f258f5a371c9886c041b0506ea4475
Farhanabd05 <13523042@std.stei.itb.ac.id>
2025-05-09 11:32:10 +0700

    fix: main.go

--- backend/test/main.go
+++ backend/test/main.go
@@ -43,14 +43,45 @@ type RecipeType struct {
 }
 
 func getElementType(index int) ElementType {
-	elementTypes := []ElementType{
-		Starting, "", Tier1, Tier2, Tier3, Tier4, Tier5, Tier6, Tier7,
-		Tier8, Tier9, Tier10, Tier11, Tier12, Tier13, Tier14, Tier15,
-	}
-	if index < 1 || index > len(elementTypes) {
+	switch index {
+	case 1:
+		return Starting
+	case 2:
+		// Skip (Ruins/Archeologist)
+		return ""
+	case 3:
+		return Tier1
+	case 4:
+		return Tier2
+	case 5:
+		return Tier3
+	case 6:
+		return Tier4
+	case 7:
+		return Tier5
+	case 8:
+		return Tier6
+	case 9:
+		return Tier7
+	case 10:
+		return Tier8
+	case 11:
+		return Tier9
+	case 12:
+		return Tier10
+	case 13:
+		return Tier11
+	case 14:
+		return Tier12
+	case 15:
+		return Tier13
+	case 16:
+		return Tier14
+	case 17:
+		return Tier15
+	default:
 		return ""
 	}
-	return elementTypes[index-1]
 }
 
 func ScrapeHandler(ctx *gin.Context) {
